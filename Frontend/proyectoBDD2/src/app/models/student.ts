1101413517fd52ef6e87845a0755e8644a128ebf
Nicolas Lorenzo <nicolaslorenzo@MacBook-Pro-de-Nicolas.local>
2024-05-22 17:48:04 -0300

    user model added

---
+++ Frontend/proyectoBDD2/src/app/models/student.ts
@@ -0,0 +1,20 @@
+export class Student {
+    ci: Number;
+    firstName : string;
+    lastName : string;
+    email : string;
+    password : string
+    championTeamID : string
+    subChampionTeamId : string
+
+    constructor(ci: Number, firstName : string, lastName : string, email : string, password : string, championTeamID : string, subChampionTeamId : string) {
+        this.ci = ci;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.championTeamID = championTeamID
+        this.subChampionTeamId = subChampionTeamId
+    }
+}
+
