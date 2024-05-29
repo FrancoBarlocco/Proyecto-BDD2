702c147a193515063ac2f3c12c372c29e677eef5
Franco Barlocco <francobarlocco@MacBook-Air-de-Franco.local>
2024-05-29 18:26:31 -0300

    cambios matchs and teams

--- proyectoBDD2/src/app/services/matchService.ts
+++ proyectoBDD2/src/app/services/matchService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Match } from '../models/match';
+import { MatchAndTeams } from '../models/matchAndTeams';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable({
@@ -17,5 +18,9 @@ export class MatchService {
 
   getMatchById(id: number): Promise<Match> {
     return firstValueFrom(this.http.get<Match>(`${this.apiUrl}/getMatchById/${id}`));
+  }
+
+  getMatchesAndTeams(): Promise<MatchAndTeams[]> {
+    return firstValueFrom(this.http.get<MatchAndTeams[]>(this.apiUrl + '/getMatchesAndTeams'));
   }
 }
