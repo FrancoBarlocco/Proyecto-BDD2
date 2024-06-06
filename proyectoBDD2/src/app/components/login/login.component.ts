87ae98dc4c5320dc21fbf45b6eadd09b311e9ed7
Franco Barlocco <francobarlocco@MacBook-Air-de-Franco.local>
2024-06-06 19:15:08 -0300

    ahora anda, fallaba cuando se guardaba en el localstorage

--- proyectoBDD2/src/app/components/login/login.component.ts
+++ proyectoBDD2/src/app/components/login/login.component.ts
@@ -49,7 +49,7 @@ export class LoginComponent {
             Contact: response.Contact
         };
           this.loginService.setUser(userResponse);
-          localStorage.setItem('user', response.userId);
+          localStorage.setItem('userId', response.userId);
           //console.log(localStorage.getItem("user"));
           if(response.userType == 'admin') //es admin
           {
