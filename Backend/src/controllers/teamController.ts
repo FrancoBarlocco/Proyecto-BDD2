733a0871cfe5e802880ddb09c4d8c8fa9d135f49
Nicolas Lorenzo <nicolaslorenzo@192.168.1.5>
2024-06-15 20:32:12 -0300

    resolve conflicts

--- Backend/src/controllers/teamController.ts
+++ Backend/src/controllers/teamController.ts
@@ -3,19 +3,6 @@ import { Request, Response } from 'express';
 
 class teamController {
 }
-
-    export const getTeamById = async (req: Request, res: Response) => {
-        const { id } = req.params; // Asume que el id viene como parámetro de ruta
-        try {
-          const results = await connection.query('SELECT * FROM Team WHERE TeamId = ?', [id]);
-           console.log(results);
-            res.json(results[0]);
-          }
-        catch (error) {
-          console.error('Error al ejecutar la consulta:', error);
-          res.status(500).json({ msg: 'Error interno del servidor' });
-        }
-      }
 export const getTeams = async (req: Request, res: Response) => {
   try {
     const [results]: any[] = await connection.query('SELECT * FROM Team');
--- Backend/src/routes/generalRoutes.ts
+++ Backend/src/routes/generalRoutes.ts
@@ -1,26 +1,19 @@
 import { Router } from 'express';
-import { getStudentByCi } from '../controllers/studentController';
-import { getStudents } from '../controllers/studentController';
-import { getTeamById } from '../controllers/teamControllet';
-import { getTeams } from '../controllers/teamControllet';
 import { getMatchesAndTeams } from '../controllers/matchController';
 import { registerUser } from '../controllers/acessController';
 import { login } from '../controllers/acessController';
 import { postMatch } from '../controllers/matchController';
 import { updateMatchResult } from '../controllers/matchController';
-import { getStadiumById } from '../controllers/stadiumController';
 import { getStadiums } from '../controllers/stadiumController';
 import { getRanking } from '../controllers/rankingController';
 import { getPredictions } from '../controllers/predictController';
 import { getStatisticsByCareer } from '../controllers/statisticController';
 import { savePredictions } from '../controllers/predictController';
+import { getTeams } from '../controllers/teamController';
 
 const { check, validationResult } = require('express-validator');
 const router = Router();
 
-router.get('/getStudentByCi/:ci',[check('Ci').matches(/^[0-9]+$/),check('Email').isEmail()], getStudentByCi);
-router.get('/getStudents', getStudents);
-router.get('/getTeamById/:id',[check('id').matches(/^[0-9]+$/)], getTeamById);
 router.get('/getTeams', getTeams);
 router.get('/getMatchesAndTeams', getMatchesAndTeams);
 router.post('/postStudent', [check('Ci').matches(/^[0-9]+$/),check('Email').isEmail()], registerUser);
@@ -29,7 +22,6 @@ router.post('/postMatch', postMatch);
 router.post('/updateMatch/:matchId',[check('matchId').matches(/^[0-9]+$/)], updateMatchResult);
 router.post('/savePredictions', savePredictions);
 router.get('/getPredictions/:userId',[check('userId').matches(/^[0-9]+$/)], getPredictions);
-router.get('/getStadiumsById/:id',[check('id').matches(/^[0-9]+$/)], getStadiumById);
 router.get('/getStadiums', getStadiums);
 router.get('/getRanking', getRanking);
 router.get('/getStatistics', getStatisticsByCareer);
